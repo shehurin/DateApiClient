fa234e9571827e7e7b7c9bcd5bfe5e9be1089319
shehurin <shehurin@gmail.com>
2019-10-17 20:02:22 +0300

    Add project

---
+++ ClientWebApi/DateApiWorker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace ClientWebApi
+{
+    class DateApiWorker
+    {
+        public string DateFrom { get; set; }
+        public string DateTo { get; set; }
+        private static readonly HttpClient client = new HttpClient();
+
+        public DateApiWorker(string dateFrom, string dateTo)
+        {
+            DateFrom = Convert.ToDateTime(dateFrom).Date.ToString("yyyy'-'MM'-'dd");
+            DateTo = Convert.ToDateTime(dateTo).Date.ToString("yyyy'-'MM'-'dd");
+        }
+
+        public async Task<bool> SendDatesAsync(string href)
+        {
+            var response = await client.PostAsync($"{href}{DateFrom}/{DateTo}", null);
+
+            return true;
+        }
+
+        public async Task<Dictionary<string, string>> GetMatchesDatesAsync(string href)
+        {
+            Dictionary<string, string> returnedResult = new Dictionary<string, string>();
+
+            var responseString = await client.GetStringAsync($"{href}{DateFrom}/{DateTo}");
+
+            dynamic stuff = JsonConvert.DeserializeObject(responseString);
+
+            foreach(var i in stuff)
+            {
+                returnedResult.Add(i.From.ToString(), i.To.ToString());
+            }
+
+
+            //string name = stuff.From;
+            //string address = stuff.To;
+
+            return returnedResult;
+        }
+
+    }
+}
---
+++ ClientWebApi/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientWebApi
+{
+    class Program
+    {
+        static string href = "http://localhost:9010/api/";
+
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo UserInput;
+            string result;
+
+            do
+            {
+                Console.WriteLine("1. Принимает интервал дат (две даты с-по) и сохраняет их в базе данных");
+                Console.WriteLine("2. Принимает интервал дат (две даты с-по) и " +
+                    "возвращает список интервалов, которые были сохранены с помощью первого веб-метода и пересекаются с ним.");
+                Console.WriteLine("3. Выход\n");
+                Console.Write("Выберите действие: ");
+
+                UserInput = Console.ReadKey();
+                result = UserInput.KeyChar.ToString();
+                Console.Clear();
+
+
+                if (result == "1")
+                {
+                    Console.Write("Введите дату \"с\" (ГГГГ-ММ-ДД): ");
+                    string dateFrom = Console.ReadLine();
+                    Console.Write("Введите дату \"по\" (ГГГГ-ММ-ДД): ");
+                    string dateTo = Console.ReadLine();
+
+
+                    DateApiWorker daw = new DateApiWorker(dateFrom, dateTo);
+                    var x = daw.SendDatesAsync(href);
+
+
+                    Console.Write("\nИнтервал дат успешно добавлен в базу данных. Нажмите любую кнопку, чтобы вернуться в меню");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else if (result == "2" )
+                {
+                    Console.Write("Введите дату \"с\" (ГГГ.ММ.ДД): ");
+                    string dateFrom = Console.ReadLine();
+                    Console.Write("Введите дату \"по\" (ГГГ.ММ.ДД): ");
+                    string dateTo = Console.ReadLine();
+
+                    Console.Write("\nСписок интервалов, с которыми были найдены пересечения:\n");
+                    ShowMatches(dateFrom, dateTo);
+                    
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                    
+
+            } while (result != "3");
+
+        }
+
+
+        public static async void ShowMatches(string dateFrom, string dateTo)
+        {
+            DateApiWorker daw = new DateApiWorker(dateFrom, dateTo);
+            Dictionary<string, string> returnedResult = await daw.GetMatchesDatesAsync(href);
+
+            if (returnedResult.Count == 0)
+                Console.WriteLine("Нет совпадений");
+
+            foreach(var i in returnedResult)
+            {
+                Console.WriteLine($"{i.Key} / {i.Value}");
+            }
+        }
+    }
+}
